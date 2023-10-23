f47cd5d810bcc5b4010edc385b249374c0b6f01c
NeedAvailableName <monkeydangluffy@gmail.com>
2023-10-23 16:37:27 +0700

    metamask

--- src/app/authentication/signup/signup.component.ts
+++ src/app/authentication/signup/signup.component.ts
@@ -9,6 +9,8 @@ import { NzFormTooltipIcon } from 'ng-zorro-antd/form';
 import { AuthenticationService } from 'src/app/service/authentication/authentication.service';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import {Router} from '@angular/router';
+import getContract from 'src/helper/contract';
+import { ethers } from 'ethers';
 
 
 @Component({
@@ -19,6 +21,7 @@ import {Router} from '@angular/router';
 export class SignupComponent implements OnInit {
   validateForm!: UntypedFormGroup;
   requestRegisterForm = {
+    contractId: '',
     username: String,
     email: String,
     password: String,
@@ -36,22 +39,41 @@ export class SignupComponent implements OnInit {
     private router: Router
   ) {}
 
-  submitForm(): void {
+  async submitForm(): Promise<void> {
     if (this.validateForm.valid) {
       console.log('submit', this.validateForm.value);
+      if(this.requestRegisterForm.role.toString() == "bank") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        provider.send('eth_requestAccounts', []);
+        const signer = provider.getSigner();
+        const contract = getContract();
+        const bankAddress = signer.getAddress();
+        await contract.addBank(this.requestRegisterForm.username, bankAddress);
+        
+        // this.requestRegisterForm.contractId = parseInt(bankID.value._hex, 16);
+        contract.on("BankAdded", (result) => {
+          // const res = new String(parseInt(result._hex, 16).toString())
+          this.validateForm.value.contractId = parseInt(result._hex, 16).toString();
+          console.log(this.validateForm.value.contractId);
+          
+        })
+      }
       this.requestRegisterForm.username = this.validateForm.value.nickname;
       this.requestRegisterForm.email = this.validateForm.value.email;
       this.requestRegisterForm.password = this.validateForm.value.password;
       this.requestRegisterForm.role = this.validateForm.value.role;
+      this.requestRegisterForm.contractId = this.validateForm.value.contractId;
       console.log(this.requestRegisterForm);
-      this.registerService
-        .register(this.requestRegisterForm)
-        .subscribe((res) => {
-          if (res.message == 'Register successfully') {
-            this.mess.success('Register successfully!');
-            this.router.navigate(['/auth/login']);
-          } else this.mess.error('Register unsuccessfully!');
-        });
+      if(this.requestRegisterForm.contractId != "") {
+        this.registerService
+          .register(this.requestRegisterForm)
+          .subscribe((res) => {
+            if (res.message == 'Register successfully') {
+              this.mess.success('Register successfully!');
+              this.router.navigate(['/auth/login']);
+            } else this.mess.error('Register unsuccessfully!');
+          });
+      } 
     } else {
       Object.values(this.validateForm.controls).forEach((control) => {
         if (control.invalid) {
@@ -86,6 +108,7 @@ export class SignupComponent implements OnInit {
 
   ngOnInit(): void {
     this.validateForm = this.fb.group({
+      contractId: '',
       email: [null, [Validators.email, Validators.required]],
       password: [null, [Validators.required]],
       checkPassword: [null, [Validators.required, this.confirmationValidator]],
