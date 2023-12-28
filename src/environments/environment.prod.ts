ed9779e374c41fc772fbd740fff56d8d750f418e
LeHa2505 <leha25052001@gmail.com>
2023-12-28 14:15:56 +0700

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,6 @@ export const environment = {
     BASE_LETTER_OF_CREDIT_API: 'https://b4lc-testv1.onrender.com/letterofcredits',
     BASE_INVOICE_API: 'https://b4lc-testv1.onrender.com/invoices',
     BASE_SERVICE_API: 'https://b4lc-testv1.onrender.com',
+    BASE_SERVICE_TEST_API: 'https://b4lc-testv2.onrender.com',
   }, 
 };
